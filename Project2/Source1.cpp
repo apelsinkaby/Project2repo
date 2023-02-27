dafdd85b488590de1aecfb1b4597318398e5f076
apelsinkaby <abstraktny228@gmail.com>
2023-02-27 18:24:42 +0300

    source1.cpp with rand_init and convert

--- Project2/Source1.cpp
+++ Project2/Source1.cpp
@@ -14,7 +14,7 @@ string convert(int* array, int size) {
 	string msg = " ";
 
 	for (int i = 0; i < size; i++) {
-		msg += to_string(array[i]) + " ";
+		msg += to_string(array[i]) + " "; 
 	}
 
 	return msg; 
